59cae4c98977404846176f9493a3f547a830a856
Amrendra Kumar <amrendra.kumar@careerbuilder.com>
2021-06-11 09:40:24 -0400

    l

--- projects/pixel/index.js
+++ projects/pixel/index.js
@@ -17,18 +17,21 @@ app.get('/collect', function (req, res) {
   res.send();
 });
 
+app.get('/setup', function (req, res) {
+  var tag = micro.now().toString(36);
+  res.cookie(cookie_tag, tag, { httpOnly: true, sameSite: 'lax' });
+  res.set('Content-Type', 'text/html');
+  res.send('<iframe height="200" width="300" style="border:0;" src="http://localhost:3000/tracker.html"></iframe>');
+});
+
 app.get('/track', function (req, res) {
 
   var host = 'localhost:3000';
   const simple_etag = 'zXJtL/PG9CWr9mi2KoRu/Ka/1R0';
   const tracking_cookie_tag = req.cookies[cookie_tag];
 
-  if (tracking_cookie_tag) {
-    // setup tracking pixel.
-    res.clearCookie(cookie_tag);
-    return renderTrackingPixel(res, simple_etag + '-' + tracking_cookie_tag)
-  }
   var tracking_etag = req.get('If-None-Match');
+
   if (tracking_etag) {
     const refererURL = new URL(req.get('Referer') ?? 'http://fake.url');
     //console.log(refererURL);
@@ -37,26 +40,33 @@ app.get('/track', function (req, res) {
       // already tracked.
       console.log('Tracking Tag: ', 'None');
       return res.status(304).send();
-    } else if (!refererURL.search) {
-      console.log('Tracking Tag: ', refererURL.host);
-      // reloading cb_page that has setup the tracker pixel should not trigger reconcilation.
-      return res.status(304).send();
+      // } else if (!refererURL.search) {
+      //   console.log('Tracking Tag: ', refererURL.host);
+      //   // reloading cb_page that has setup the tracker pixel should not trigger reconcilation.
+      //   return res.status(304).send();
     } else {
+      if (tracking_cookie_tag) {
+        return res.status(304).send();
+      }
       var tag = tracking_etag.replace(simple_etag + '-', '');
       // Send for reconcilation
       console.log('Tracking Tag: ', tag);
       console.log('Reconcilation triggered for tagId: ', tag);
+      res.clearCookie(cookie_tag);
       return renderTrackingPixel(res, simple_etag);
     }
   } else {
-    console.log('Tracking Tag: ', 'No-Cache');
-    // user has disabled cache, we can't track using etag.
-    // TODO: What option do we have?
     var tag = '';
-    if (req.get('Cache-Control') && req.get('Cache-Control').indexOf('no-cache') < 0) {
+    if (tracking_cookie_tag) {
+      // setup tracking pixel.
+      tag = '-' + tracking_cookie_tag;
+    } else if (req.get('Cache-Control') && req.get('Cache-Control').indexOf('no-cache') < 0) {
+      // user has disabled cache, we can't track using etag.
+      // TODO: What option do we have?
+      console.log('Tracking Tag: ', 'No-Cache');
       tag = '-' + micro.now().toString(36);
+
     }
-    // loading non tracking pixel image.
     return renderTrackingPixel(res, simple_etag + tag);
   }
 });
